9a2e719982392c267fa95c680dbbe483ad3cea97
Rajiv Khushiram <31241377+Rajiv-Khushiram@users.noreply.github.com>
2019-11-03 23:21:30 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Rajiv Khushiram
         </a>
       </header>
     </div>
