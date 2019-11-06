78250508fea293cbe97e57f340c142fdccb3ecd2
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-11-07 08:29:09 +1100

    removed function hellowworld

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+// admin.initializeApp(functions.config().firebase);
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+// // export const helloWorld = functions.https.onRequest((request, response) => {
+// //  response.send("Hello from Firebase!");
+// // });
