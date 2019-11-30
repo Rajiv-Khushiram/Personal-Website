a3634e38687693fba00550d6975e9359052af207
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-12-01 00:11:30 +1100

    hide buttons for now

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -65,27 +65,31 @@ class Blog extends Component {
     const { blog } = this.props;
     return (
       <React.Fragment>
-        <Header background="https://firebasestorage.googleapis.com/v0/b/raj-c-k.appspot.com/o/joshua-earle-K3V1WUkqBxM-unsplash.jpg?alt=media&token=f9edb71d-84cd-4b0f-8d56-469d5d6e9a74&fbclid=IwAR11x6B2Xdh_pK1yPZ3n-O9B6nLXP_NAax036kunYpASCqXVwBfX3RSqTM4" heading="Raj-K"  blog iconWrapper subtitle="Think before you speak. Read before you think."/>
-        <Menu/>
+        <Header
+          background="https://firebasestorage.googleapis.com/v0/b/raj-c-k.appspot.com/o/joshua-earle-K3V1WUkqBxM-unsplash.jpg?alt=media&token=f9edb71d-84cd-4b0f-8d56-469d5d6e9a74&fbclid=IwAR11x6B2Xdh_pK1yPZ3n-O9B6nLXP_NAax036kunYpASCqXVwBfX3RSqTM4"
+          heading="Raj-K"
+          blog
+          iconWrapper
+          subtitle="Think before you speak. Read before you think."
+        />
+        <Menu />
         <WrapperLeft>
-          <Raj/>
+          <Raj />
         </WrapperLeft>
-        <BlogSection id='about'>
-          <Bloglist  blogs={blog} />
-          <Button
-                    onClick={() => this.previous()}
-                  >
-                    Previous
-          </Button>
-          <Button
-                    style={{float:"right"}}
-                    onClick={() => this.next()}
-                  >
-                    Next
-          </Button>
+        <BlogSection id="about">
+          <Bloglist blogs={blog} />
+          {blog.length > 2 && (
+          <div>
+          <Button onClick={() => this.previous()}>Previous</Button>
+
+            <Button style={{ float: "right" }} onClick={() => this.next()}>
+              Next
+            </Button></div>
+          )}
+
           <br></br>
         </BlogSection>
-       <Footer background="https://firebasestorage.googleapis.com/v0/b/raj-c-k.appspot.com/o/joshua-earle-K3V1WUkqBxM-unsplash.jpg?alt=media&token=f9edb71d-84cd-4b0f-8d56-469d5d6e9a74&fbclid=IwAR11x6B2Xdh_pK1yPZ3n-O9B6nLXP_NAax036kunYpASCqXVwBfX3RSqTM4"/>
+        <Footer background="https://firebasestorage.googleapis.com/v0/b/raj-c-k.appspot.com/o/joshua-earle-K3V1WUkqBxM-unsplash.jpg?alt=media&token=f9edb71d-84cd-4b0f-8d56-469d5d6e9a74&fbclid=IwAR11x6B2Xdh_pK1yPZ3n-O9B6nLXP_NAax036kunYpASCqXVwBfX3RSqTM4" />
       </React.Fragment>
     );
   }
