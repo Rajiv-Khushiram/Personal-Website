e30698baeaa976b78c1865c85762ea7b3e4a7e37
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-12-02 02:53:39 +1100

    Algolia Functions: CRUD on Blogs Collection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,45 @@ const ALGOLIA_INDEX_BLOGS = "blogs";
 
 admin.initializeApp(functions.config().firebase);
 
+exports.indexBlog = functions.region("asia-northeast1")
+    .firestore.document("blogs/{blogId}")
+    .onCreate((snap, context) => {
+        // Get the note document
+        const blog = snap.data();
+        // Add an 'objectID' field which Algolia requires
+        if (blog) {
+            blog.objectID = context.params.blogId;
+            const index = client.initIndex(ALGOLIA_INDEX_BLOGS);
+            return index.saveObject(blog);
+        }
+        console.log("Algolia: Could not index blog indice")
+        return;
+    });
 
+exports.updateBlog = functions.firestore.document("blogs/{blogId}")
+    .onUpdate((change, context) => {
+        const blog = change.after.data();
+        if (blog) {
+            blog.objectID = context.params.blogId;
+            const index = client.initIndex(ALGOLIA_INDEX_BLOGS);
+            return index.saveObject(blog);
+        }
+        console.log("Algolia: Could not update blog indice")
+        return;
+    });
+
+exports.unIndexBlog = functions.region("asia-northeast1")
+    .firestore.document("blogs/{blogId}")
+    .onDelete((snap, context) => {
+        const blog = snap.data();
+        if (blog) {
+            blog.objectID = String(context.params.blogId);
+            const index = client.initIndex(ALGOLIA_INDEX_BLOGS);
+            return index.deleteObject(blog.objectID);
+        }
+        console.log("Algolia: Could not delete blog indice")
+        return;
+    });
 
 // admin.initializeApp(functions.config().firebase);
 
